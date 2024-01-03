a02e1f129d58438bf1c8b82276baadc5ed548527
aaron <95997167+Free-Aaron-Li@users.noreply.github.com>
2024-01-03 21:26:24 +0800

    🛠  refactor(src/): Add cmake environment architecture

---
+++ src/PartI/ChapterI/src/main.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2024. aaron.
+//
+// This program is under the GPL-3.0 license.
+// if you have not received it or the program has a bug, please let me know:
+// <communicate_aaron@outlook.com>.
+
+// This file is part of 'PartI' project.
+// This program is called:'main'.
+// If there are no special instructions, this file is used as an exercise and test file.
+#include <iostream>
+
+int
+main() {
+    std::cout << "hello";
+}
