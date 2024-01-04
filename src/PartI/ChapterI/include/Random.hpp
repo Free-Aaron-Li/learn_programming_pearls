789c7e2be44466bfd987f73bfa9ac5e4ddabee4d
aaron <95997167+Free-Aaron-Li@users.noreply.github.com>
2024-01-04 16:39:05 +0800

    📚  docs(Random hpp): add comment
    Modify (add) comments to member functions and member variables of the Random class

--- src/PartI/ChapterI/include/Random.hpp
+++ src/PartI/ChapterI/include/Random.hpp
@@ -25,6 +25,13 @@
 
 class Random {
  public:
+    /**
+     * @brief Random
+     * @param quantity The random number quantity
+     * @param min_num The lowest range value of the random number
+     * @param max_num The highest range value of the random number
+     * @return void
+     */
     explicit Random(const long quantity = 0, const long min_num = 0, const long max_num = 0)
         : _num_quantity(quantity),
           _min_num(min_num),
@@ -33,19 +40,27 @@ class Random {
 
     /**
      * @brief Random number
-     * @param quantity the random number quantity
-     * @param min_num the lowest range value of the random number
-     * @param max_num the highest range value of the random number
-     * @return void
+     * @param quantity The random number quantity
+     * @param min_num The lowest range value of the random number
+     * @param max_num The highest range value of the random number
+     * @return _numbers
      */
     void number(long quantity, long min_num, long max_num);
 
+    /**
+     * @brief Print the numbers
+     * @return outfile
+     */
     void printNumber();
 
  private:
+    /// @param _numbers A container for storing numbers
     std::shared_ptr<std::vector<long>> _numbers;
+    /// @param _num_quantity Digital total
     long                               _num_quantity;
+    /// @param _min_num Numeric Value Minimum interval value
     long                               _min_num;
+    /// @param _max_num Numeric Value Maximum interval value
     long                               _max_num;
 };
 #endif  // CHAPTERI_RANDOM_HPP
